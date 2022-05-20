ad5b82970c0b5bd044bbabccbc5d58fd6f58ba95
anandmali <anandm475@gmail.com>
2022-05-20 14:02:11 +0530

    Refactored logger to timber usage.

--- app/src/main/java/com/anandmali/lifecyclestate/location/BoundLocation.kt
+++ app/src/main/java/com/anandmali/lifecyclestate/location/BoundLocation.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import androidx.lifecycle.LifecycleObserver
 
 
 class BoundLocation(
--- app/src/main/java/com/anandmali/lifecyclestate/location/MyLocationListener.kt
+++ app/src/main/java/com/anandmali/lifecyclestate/location/MyLocationListener.kt
@@ -2,9 +2,10 @@ package com.anandmali.lifecyclestate.location
 
 import android.location.Location
 import android.location.LocationListener
+import timber.log.Timber
 
 class MyLocationListener : LocationListener {
     override fun onLocationChanged(p0: Location) {
-        println("onLocationChanged => $p0")
+        Timber.i("onLocationChanged => $p0")
     }
 }
