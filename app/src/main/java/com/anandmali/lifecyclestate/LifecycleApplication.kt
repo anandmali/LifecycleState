fa970b0f6c736d593f9bff17761cc8432007ebe3
anandmali <anandm475@gmail.com>
2022-05-21 14:20:13 +0530

    Enabling timber debug tree

--- app/src/main/java/com/anandmali/lifecyclestate/LifecycleApplication.kt
+++ app/src/main/java/com/anandmali/lifecyclestate/LifecycleApplication.kt
@@ -3,6 +3,7 @@ package com.anandmali.lifecyclestate
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import androidx.viewbinding.BuildConfig
 import timber.log.Timber
 
 class LifecycleApplication : Application(), Application.ActivityLifecycleCallbacks {
@@ -12,6 +13,10 @@ class LifecycleApplication : Application(), Application.ActivityLifecycleCallbac
     override fun onCreate() {
         super.onCreate()
         registerActivityLifecycleCallbacks(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     override fun onActivityCreated(p0: Activity, p1: Bundle?) {
